03a0f4143808378bdac94c4acc0b242212c737a2
DESKTOP-6HF7UMR\VerDer-Geon <vertice_v@hotmail.com>
2018-02-27 11:21:43 +0700

    first commit

---
+++ src/main/java/com/example/websocketdemo/model/Request.java
@@ -0,0 +1,20 @@
+package com.example.websocketdemo.model;
+
+public class Request {
+    private String name;
+
+    public Request() {
+    }
+
+    public Request(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
